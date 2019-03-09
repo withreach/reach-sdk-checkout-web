864fdd0250487a517fb33ca5fb63e9bf69b70804
Landon Boyd <landon@withreach.com>
2019-03-09 00:36:14 -0500

    [DEV-3619] Update test js.

--- test/site.js
+++ test/site.js
@@ -1,22 +1,17 @@
 var merchantId = '18da9ea3-f9ac-4e64-8405-d301f079a658';
 var fingerprint = '5b421716-5117-41c5-93a8-e5b300c8496a';
-
+var transaction = '6e2e1b26-7b34-43e6-b240-b2c7cb85748f';
 
 rch.requirements(merchantId, { Currency: 'EUR',
                                DeviceFingerprint: fingerprint })
 .then(function(result) {
   console.log('requirements success: ' + JSON.stringify(result));
+  return rch.challenge("https://checkout-dev.rch.io/challenge/" + transaction, 
+                       "03", document.getElementById("container"));
 })
-.catch(function(error) {
-  console.log('requirements failure: ' + error);
-});
-
-rch.challenge("https://checkout.rch.red/challenge/95189a69-defd-48e3-997a-6f8dca8d16d8", 
-              "03", document.getElementById("container"))
 .then(function(result) {
   console.log("challenge done: " + JSON.stringify(result))
 })
 .catch(function(error) {
-  console.log('challenge failure: ' + error);
+  console.log('failure: ' + error);
 });
-
