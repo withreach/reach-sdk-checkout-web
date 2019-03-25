1408b9b0e3ffcdbca59792f8aff555db9d3c099f
Landon Boyd <landon@withreach.com>
2019-03-25 16:08:40 -0400

    [DEV-3619] Improve test client, remove hardcoded parameters.

--- test/site.js
+++ test/site.js
@@ -1,8 +1,15 @@
-var merchantId = '18da9ea3-f9ac-4e64-8405-d301f079a658';
-var transaction = '6e2e1b26-7b34-43e6-b240-b2c7cb85748f';
 
-console.log('requirements success: ' + JSON.stringify(result));
-rch.challenge("https://engine-dev.gointerpay.net/challenge/" + transaction, 
-              "03", document.getElementById("container"), function (result) {
-  console.log("result: " + JSON.stringify(result))
-});
+
+document.getElementById("challenge").onclick = function(e) {
+  console.log("hello");
+  rch.challenge(document.getElementById("url").value, 
+                document.getElementById("windowSize").value,
+                document.getElementById("container"), 
+                function (result) {
+    document.getElementById("result").value 
+      = Date() + "\n" + JSON.stringify(result);
+  });
+  
+  
+}
+
